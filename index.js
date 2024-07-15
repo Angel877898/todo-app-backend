97d51b6aab9e2f4c8c476e49dd200e1944f17511
Angel877898 <50708206+Angel877898@users.noreply.github.com>
2024-07-15 17:15:46 -0600

    feat:adding mongodb

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const connectDB = require('./config/db');
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+// Conect to MongoDB
+connectDB();
 
 app.use(bodyParser.json());
 app.use(cors());
